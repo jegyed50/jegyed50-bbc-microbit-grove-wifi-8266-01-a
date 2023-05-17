f14b3fd39be749dfbb578b264f300bfb63c6b0a1
Egyed József <jegyed50@gmail.com>
2023-05-17 10:42:17 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,28 @@
-//jegyed50-bbc-microbit-Grove-WiFi-8266-01-A
+esp8266.init(SerialPin.P16, SerialPin.P15, BaudRate.BaudRate115200)
+if (esp8266.isESP8266Initialized()) {
+    esp8266.connectWiFi("TP-LINK_WiFi_2", "Kajdacs1")
+    basic.showLeds(`
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    if (esp8266.isWifiConnected()) {
+        basic.showLeds(`
+            # # . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+} else {
+    basic.showIcon(IconNames.No)
+}
+// jegyed50-bbc-microbit-Grove-WiFi-8266-01-A
 basic.forever(function () {
 	
 })
