e5890c62b9b095a0a9c9dbf0702a377ea815aa3a
Egyed József <jegyed50@gmail.com>
2023-05-23 18:19:32 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,42 @@
+input.onButtonPressed(Button.A, function () {
+    esp8266.init(SerialPin.P16, SerialPin.P15, BaudRate.BaudRate115200)
+    esp8266.init(SerialPin.P16, SerialPin.P15, BaudRate.BaudRate115200)
+    if (esp8266.isESP8266Initialized()) {
+        esp8266.connectWiFi("TP-LINK_WiFi_2", "Kajdacs1")
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        if (esp8266.isWifiConnected()) {
+            basic.showLeds(`
+                # # . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+            esp8266.sendTelegramMessage("5874557565:AAFtysuEtoL3dDpyvUgItQyj9eayF3YCQlg", "5970729327 ", "ALARM the door is open")
+            if (esp8266.isTelegramMessageSent()) {
+                basic.showLeds(`
+                    # # # . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    `)
+            } else {
+                basic.showIcon(IconNames.No)
+            }
+        } else {
+            basic.showIcon(IconNames.No)
+        }
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+})
 esp8266.init(SerialPin.P16, SerialPin.P15, BaudRate.BaudRate115200)
 if (esp8266.isESP8266Initialized()) {
     esp8266.connectWiFi("TP-LINK_WiFi_2", "Kajdacs1")
@@ -16,7 +55,7 @@ if (esp8266.isESP8266Initialized()) {
             . . . . .
             . . . . .
             `)
-        esp8266.sendTelegramMessage("5874557565:AAFtysuEtoL3dDpyvUgItQyj9eayF3YCQlg", "5970729327 ", "Test 03")
+        esp8266.sendTelegramMessage("5874557565:AAFtysuEtoL3dDpyvUgItQyj9eayF3YCQlg", "5970729327 ", "Home alarm system is ON")
         if (esp8266.isTelegramMessageSent()) {
             basic.showLeds(`
                 # # # . .
@@ -34,7 +73,3 @@ if (esp8266.isESP8266Initialized()) {
 } else {
     basic.showIcon(IconNames.No)
 }
-// jegyed50-bbc-microbit-Grove-WiFi-8266-01-A
-basic.forever(function () {
-	
-})
